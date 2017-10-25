23320d7e6249a178cf56fd212715e2cec34edeb4
HannesRaschke <Hannes.raschke@student.reutlingen-university.de>
2017-10-25 21:03:49 +0200

    styled the chat, removed node modules (run package.json after pull!)

--- Chat_Group_G/index.js
+++ Chat_Group_G/index.js
@@ -1,3 +1,10 @@
+/////////////////////
+//Dennis Funk
+//Hannes Raschke
+//Group G
+//
+
+
 var express = require('express');
 var app = express();
 var http = require('http').Server(app);
@@ -6,143 +13,190 @@ var port = process.env.PORT || 3000;
 var path = require('path');
 var fs = require('fs')
 
-var users={};
+var users = {};
 
-//app.use(express.static(__dirname+"/public/Temp/*"))
+// app.use(express.static(__dirname+"/public/Temp/*"))
 var bodyParser = require('body-parser');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
-    extended: true
+	extended : true
 }));
 
-
-app.get('/', function(req, res){
-  res.sendFile(__dirname + '/public/index.html');
+app.get('/', function(req, res) {
+	res.sendFile(__dirname + '/public/index.html');
 });
 
-app.use(express.static(path.join(__dirname, 'public'))); 
+app.use(express.static(path.join(__dirname, 'public')));
 
 // //////////////////////////////////////////////////////////////
-io.on('connection', function(socket){
-	
-	//on chatmessage, send to all clients
-	socket.on('chat message', function(msg){
+io.on('connection', function(socket) {
+
+	// on chatmessage, send to all clients
+	socket.on('chat message', function(msg) {
 		console.log(__dirname)
 		msg.timestamp = timestamp();
 		io.emit('chat message', msg);
 	});
 
-	//if a command is send, check what kind of command
-	socket.on('command', function(msg){
-		msgElements=msg.content.split(" ");
-		var flag=msgElements[0];
-		if(flag==="\\pm"){
-			var receiver=users[msgElements[1]];
-			if(receiver===undefined){
-				io.to(users[msg.id]).emit('private message',{});
-			}else{
-				for(var id in users){
-					if(receiver===users[id]){
-						//saving receivers name
+	// if a command is send, check what kind of command
+	socket.on('command', function(msg) {
+		msgElements = msg.content.split(" ");
+		var flag = msgElements[0];
+		if (flag === "\\pm") {
+			var receiver = users[msgElements[1]];
+			if (receiver === undefined) {
+				io.to(users[msg.id]).emit('private message', {});
+			} else {
+				for ( var id in users) {
+					if (receiver === users[id]) {
+						// saving receivers name
 						var receiverName = id;
-						//getting message 
-						var message = msg.content.substring(msgElements[0].length+msgElements[1].length+2);
-				//sending to receiver
-				io.to(users[id]).emit('private message', {from:socket.nickname  , to:receiverName, message:message, timestamp:timestamp()})
-				//sending to sender
-				socket.emit('private message', {from:socket.nickname  , to:receiverName, message:message, timestamp:timestamp()})
-											}
-									}
-				 }
+						// getting message
+						var message = msg.content
+								.substring(msgElements[0].length
+										+ msgElements[1].length + 2);
+						// sending to receiver
+						io.to(users[id]).emit('private message', {
+							from : socket.nickname,
+							to : receiverName,
+							message : message,
+							timestamp : timestamp()
+						})
+						// sending to sender
+						socket.emit('private message', {
+							from : socket.nickname,
+							to : receiverName,
+							message : message,
+							timestamp : timestamp()
+						})
+					}
+				}
 			}
-		else if(flag==="\\list"){
+		} else if (flag === "\\list") {
 			var userList = "Following users are online: ";
 			userList += Object.keys(users);
-			socket.emit('command', {timestamp:timestamp(), content:userList})
-		}else{
-			message="This command does not exist. Try \\list , \\pm";
-			socket.emit('command', {timestamp:timestamp(), content:message})
-			
-			}
+			socket.emit('command', {
+				timestamp : timestamp(),
+				content : userList
+			})
+		} else {
+			message = "This command does not exist. Try \\list , \\pm";
+			socket.emit('command', {
+				timestamp : timestamp(),
+				content : message
+			})
+
+		}
 	});
-	
-	//on client enter save nickname and send join message to clients
+
+	// on client enter save nickname and send join message to clients
 	socket.on('clientEnterEvent', function(nick) {
 		if (/^\w+$/.test(nick)) {
-		//if nick does not exist yet
-			if(!(nick in users)){
-	    // connects the user to their socket id (acts as a cookie)
-		users[nick]=socket.id;
-	    socket.emit('enter', nick);
-	    //Save nickname on socket
-	    socket.nickname=nick;
-	    io.emit('system message', {action:" joined", timestamp:timestamp(), user:socket.nickname});
-	    var OnlineUser = Object.keys(users);
-		io.emit('OnlineUserWidget', {content:OnlineUser});
-		}else{
-                var taken = true;
-                socket.emit('nickTaken', taken);
-                }
-        }else{
-            var failure = true;
-            socket.emit('invalidNick', failure);
-        }
+			// if nick does not exist yet
+			if (!(nick in users)) {
+				// connects the user to their socket id (acts as a cookie)
+				users[nick] = socket.id;
+				socket.emit('enter', nick);
+				// Save nickname on socket
+				socket.nickname = nick;
+				io.emit('system message', {
+					action : " joined",
+					timestamp : timestamp(),
+					user : socket.nickname
+				});
+				var OnlineUser = Object.keys(users);
+				io.emit('OnlineUserWidget', {
+					content : OnlineUser
+				});
+			} else {
+				var taken = true;
+				socket.emit('nickTaken', taken);
+			}
+		} else {
+			var failure = true;
+			socket.emit('invalidNick', failure);
+		}
 	});
 
 	// on disconnect delete user data and send "user disconnected" message
-	socket.on('disconnect', function(){
-		io.emit('system message', {action:" left", timestamp:timestamp(), user:socket.nickname});
+	socket.on('disconnect', function() {
+		io.emit('system message', {
+			action : " left",
+			timestamp : timestamp(),
+			user : socket.nickname
+		});
 		delete users[socket.nickname];
 		var OnlineUser = Object.keys(users);
-		io.emit('OnlineUserWidget', {content:OnlineUser});
+		io.emit('OnlineUserWidget', {
+			content : OnlineUser
+		});
 	});
-	
-	//gets a file object and uses an fs stream to write it to a file. Then sends the file as a file message to all users
-	socket.on('upload',function(file){
+
+	// gets a file object and uses an fs stream to write it to a file. Then
+	// sends the file as a file message to all users
+	socket.on('upload', function(file) {
 		console.log(file)
-		
-		var stream = fs.createWriteStream("public/Temp/"+file.fileName)
-		stream.once('open',function(){
+
+		var stream = fs.createWriteStream("public/Temp/" + file.fileName)
+		stream.once('open', function() {
 			stream.write(file.file);
 			stream.end();
 		})
-		var time =timestamp()
-		if(file.isPrivate){
-			
-			msgElements=file.content.split(" ");
-			var flag=msgElements[0];
-			if(flag==="\\pm"){
-				var receiver=users[msgElements[1]];
-				if(receiver===undefined){
-					io.to(users[file.id]).emit('private message',{});
-				}else{
-					for(var id in users){
-						if(receiver===users[id]){
-					io.to(users[id]).emit('file message',{timestamp:time,fileName:file.fileName,id:file.id,content:file.content});
-					socket.emit('file message',{timestamp:time,fileName:file.fileName,id:file.id,content:file.content});
+		var time = timestamp()
+		if (file.isPrivate) {
+
+			msgElements = file.content.split(" ");
+			var flag = msgElements[0];
+			if (flag === "\\pm") {
+				var receiver = users[msgElements[1]];
+				if (receiver === undefined) {
+					io.to(users[file.id]).emit('private message', {});
+				} else {
+					for ( var id in users) {
+						if (receiver === users[id]) {
+							io.to(users[id]).emit('file message', {
+								timestamp : time,
+								fileName : file.fileName,
+								id : file.id,
+								content : file.content
+							});
+							socket.emit('file message', {
+								timestamp : time,
+								fileName : file.fileName,
+								id : file.id,
+								content : file.content
+							});
 						}
 					}
 				}
-						
+
 			}
-		}else{
-			io.emit('file message',{timestamp:time,fileName:file.fileName,id:file.id,content:file.content});
+		} else {
+			io.emit('file message', {
+				timestamp : time,
+				fileName : file.fileName,
+				id : file.id,
+				content : file.content
+			});
 		}
-		
-});
+
+	});
 });
 
 // ////////////////////////////////////////////////////////////////////
 
-http.listen(port, function(){
-  console.log('listening on *:' + port);
+http.listen(port, function() {
+	console.log('listening on *:' + port);
 });
 
-function timestamp(){
-	var currentdate = new Date(); 
-  	var time = "[" + (currentdate.getHours()<10?'0':'') + currentdate.getHours()+ ":"  
-      		+ (currentdate.getMinutes()<10?'0':'') + currentdate.getMinutes() + ":" +
-      		(currentdate.getSeconds()<10?'0':'') + currentdate.getSeconds()  + "] ";
-  	return time;
-//  	msg.timestamp = timestamp();
+function timestamp() {
+	var currentdate = new Date();
+	var time = "[" + (currentdate.getHours() < 10 ? '0' : '')
+			+ currentdate.getHours() + ":"
+			+ (currentdate.getMinutes() < 10 ? '0' : '')
+			+ currentdate.getMinutes() + ":"
+			+ (currentdate.getSeconds() < 10 ? '0' : '')
+			+ currentdate.getSeconds() + "] ";
+	return time;
+	// msg.timestamp = timestamp();
 }
--- Chat_Group_G/public/js/enterJS.js
+++ Chat_Group_G/public/js/enterJS.js
@@ -1,140 +1,188 @@
+$(function() {
+	var socket = io();
+	var nick;
 
-		$(function () {
-	    	  var socket = io();
-	    	  var nick;
-	    	  
-	    	  var fileSelected
-	    	  var fileReader
-    	  ////////////////// 
-    	  //username entered; button pressed
-    	  $("#enterUsername").submit(function(){
-    	        nick = document.getElementById("userNameInp").value; 
-    	        socket.emit("clientEnterEvent", nick);
-    	        return false;
-    	    });
-    	    
-    	    
-    	    //////////////////
-        	//listen on server, chat message received
-    	    socket.on('chat message', function(msg){
-    	    	console.log("message123")
-    	    	console.log(msg);
-                	
-                	$('#messages').append($('<li>').text(msg.timestamp+msg.id+": "+msg.content));
-                window.scrollTo(0, document.body.scrollHeight);
-            });
-    	    
-    	    socket.on('private message', function(msg){
-    	    	console.log("pm123")
-    	    	console.log(msg);
-    	    	$('#messages').append($('<li id="privateMessage">').text(msg.timestamp+msg.from+" whispers to " + msg.to+ ": " +msg.message));
-    	    	window.scrollTo(0, document.body.scrollHeight);
-    	    });
-    	    
-    	    socket.on('system message', function(msg){
-    	    	console.log("sys");
-               	$('#messages').append($('<li id="systemMessage">').text(msg.timestamp + "SYSTEM: " +msg.user + msg.action));
-    	    	window.scrollTo(0, document.body.scrollHeight);
-    	    });
-    	    
-    	    socket.on('command',function(msg){
-    	    	$('#messages').append($('<li id="systemMessage">').text(msg.timestamp  +msg.content));
-    	    	window.scrollTo(0, document.body.scrollHeight);
-    	    })
-    	    
-    	    //shows normal message and the uploaded file
-    	    socket.on('file message', function(msg){ 
-//                	$('#messages').append($('<li id="fileMessage">').text(msg.timestamp+msg.id+": "+msg.content));
-                	$('#messages').append($('<a id="file" href = "Temp/'+msg.fileName+'" download>'+'<img id= "fileImg" src="Media/file.png" alt = "'+msg.fileName+'">'+'</a>')); //onclick="downloadFile('+"'"+msg.fileName+"'"+')"  //
-                window.scrollTo(0, document.body.scrollHeight);
-            });
-    	    
-    	  //////////////////
-      	  //listen on server, entered chat
-    	    socket.on('enter', function(nick){
-    	    	console.log(nick);
-//    	    		myNick=nick;
-    	    		console.log("socket on enter");
-        	    	console.log(nick);
-        	    	
-        	    	var smilies = "";
-        	    	for(var i=128512; i <= 128591; i++){
-        	    		smilies += '<span class="smilies" id="&#'+i+'">&#'+i+'</span>';
-        	    	}
-        	    	
-        	    	var chat = '<div id="smilies">'+smilies+'</div><div id="onlineUserWidget"></div><ul id="messages"></ul><form id="chat" action=""><input type="file" id="f"><div id="d"></div><input id="m" autocomplete="off" /><button id="bChat">Send</button></form>'; 
-        	            $("body").empty();
-        	            $("body").append(chat);
-        	            
-        	            document.getElementById('f').addEventListener('change', FileChosen);
-        	            
-        	      	  //////////////////
-        	      	  //send chat message; button pressed // eventhandler only after "chat" is created
-        	      	    $("#chat").submit(function(){
-        	      	    	var isPrivate;
-        	      	    	//if the message is a command
-        	     	    	if($('#m').val().charAt(0)==="\\" ){   
-        	              	  socket.emit('command',{id:nick,content:$('#m').val()})
-        	              	  isPrivate=true
-        	      	    	//normal message
-        	      	    	}else{
-        	      	    		socket.emit("chat message",{ id:nick,content:$("#m").val(),timestamp: ""});
-        	      	    		isPrivate=false
-        	      	    	}
-        	     	    	//if a file is send with the message
-        	     	    	if(fileSelected!=undefined){
-        	     	    		socket.emit('upload',{file:fileSelected,fileName:fileSelected.name,id:nick,content:$("#m").val(),isPrivate:isPrivate})//type:fileSelected.type
-    	      	    			$('#d').html("");   
-    	      	    			fileSelected=undefined;
-        	     	    	}
-        	      	          $("#m").val("");
-        	      	          return false;
-        	      	        });    	            
-        	      	  /////////////////////////
-        	      	    
-        	     	    
-          	      	  $(function(){
-          	      		  $('.smilies').on("click",function(){
-          	      			  var smilie = $(this).attr('id');
-          	      			  var chatmessage = $('#m').val();
-          	      		  $('#m').val(chatmessage + smilie );
-          	      		  })
-          	      	  })  
-          	      	  
-          	      	  $(function(){
-          	      		  $('#onlineUserWidget').on("click",".onlineUser",function(){
-        	      			  var user = $(this).attr('id');
-        	      			  var chatmessage = $('#m').val();
-        	      			  $('#m').val("\\pm " + user + " " +chatmessage);
-        	      		  })
-        	      	  })  
-    	    });
-    	    
-    	    socket.on('nickTaken', function(data){
-                alert("Username already taken!");
-            });
-            
-             socket.on('invalidNick', function(data){
-                alert("Enter a valid username\nOnly letters and numbers allowed");
-            });
+	var fileSelected
+	var fileReader
+	// ////////////////
+	// username entered; button pressed
+	$("#enterUsername").submit(function() {
+		nick = document.getElementById("userNameInp").value;
+		socket.emit("clientEnterEvent", nick);
+		return false;
+	});
 
-            //onclick function for the file upload
-            function FileChosen(event){
-    	    	fileSelected = event.target.files[0]
-    	    		$('#d').html("File Selected: "+fileSelected.name);   	
-    	    	}
-            
-            socket.on('OnlineUserWidget',function(msg){
-    	    	var OUWUsers = "";
-    	    	for(var i=0; i < msg.content.length; i++){
-    	    		OUWUsers += '<span class=onlineUser id='+msg.content[i]+'>'+msg.content[i]+'</span><br>';
-    	    	}
-    	    	
-    	    	console.log("ouw");
-    	    	console.log(msg.content);
-    	    	console.log(OUWUsers);
-    	    	$('#onlineUserWidget').empty();
-    	    	$('#onlineUserWidget').append(OUWUsers);
-    	    })
-            
-      });
+	// ////////////////
+	// listen on server, chat message received
+	socket.on('chat message', function(msg) {
+		console.log("message123")
+		console.log(msg);
+
+		$('#messages').append(
+				$('<li>').text(msg.timestamp + msg.id + ": " + msg.content));
+		$('#messages')[0].scrollTo(0, $('#messages')[0].scrollHeight);
+	});
+
+	socket.on('private message', function(msg) {
+		console.log("pm123")
+		console.log(msg);
+		$('#messages').append(
+				$('<li id="privateMessage">').text(
+						msg.timestamp + msg.from + " whispers to " + msg.to
+								+ ": " + msg.message));
+		$('#messages')[0].scrollTo(0, $('#messages')[0].scrollHeight);
+	});
+
+	socket.on('system message', function(msg) {
+		console.log("sys");
+		$('#messages').append(
+				$('<li id="systemMessage">').text(
+						msg.timestamp + "SYSTEM: " + msg.user + msg.action));
+		$('#messages')[0].scrollTo(0, $('#messages')[0].scrollHeight);
+	});
+
+	socket.on('command',
+			function(msg) {
+				$('#messages').append(
+						$('<li id="systemMessage">').text(
+								msg.timestamp + msg.content));
+				$('#messages')[0].scrollTo(0, $('#messages')[0].scrollHeight);
+			})
+
+	// shows normal message and the uploaded file
+	socket.on('file message', function(msg) {
+		// $('#messages').append($('<li
+		// id="fileMessage">').text(msg.timestamp+msg.id+": "+msg.content));
+		$('#messages').append(
+				$('<a id="file" href = "Temp/' + msg.fileName + '" download>'
+						+ '<img id= "fileImg" src="Media/file.png" alt = "'
+						+ msg.fileName + '">' + '</a>')); // onclick="downloadFile('+"'"+msg.fileName+"'"+')"
+		// //
+		$('#messages')[0].scrollTo(0, $('#messages')[0].scrollHeight);
+	});
+
+	// ////////////////
+	// listen on server, entered chat
+	socket
+			.on(
+					'enter',
+					function(nick) {
+						console.log(nick);
+						// myNick=nick;
+						console.log("socket on enter");
+						console.log(nick);
+
+						var smilies = "";
+						for (var i = 128512; i <= 128591; i++) {
+							smilies += '<span class="smilies" id="&#' + i
+									+ '">&#' + i + '</span>';
+						}
+
+						var chat = '<div id="chatWrapper"><div id="onlineUserWidget"></div><div id="chatMainFrame"><ul id="messages"></ul><form id="chat" action=""><input type="submit" id="inputSubmit"><div id="smilies">'
+								+ smilies
+								+ '</div><div id="dFileUpload"><input type="file" id="f" /><button id="bFileUpload">File attachment</button><div id="d"></div></div><input id="m" autocomplete="off" /><button id="bChat">Send</button><button id="bSmilies">&#128512</button></form></div></div>';
+						$("body").empty();
+						$("body").append(chat);
+
+						document.getElementById('f').addEventListener('change',
+								FileChosen);
+
+						// ////////////////
+						// send chat message; button pressed // eventhandler
+						// only after "chat" is created
+						$("#chat").submit(function() {
+							var isPrivate;
+							// if the message is a command
+							if ($('#m').val().charAt(0) === "\\") {
+								socket.emit('command', {
+									id : nick,
+									content : $('#m').val()
+								})
+								isPrivate = true
+								// normal message
+							} else {
+								socket.emit("chat message", {
+									id : nick,
+									content : $("#m").val(),
+									timestamp : ""
+								});
+								isPrivate = false
+							}
+							// if a file is send with the message
+							if (fileSelected != undefined) {
+								socket.emit('upload', {
+									file : fileSelected,
+									fileName : fileSelected.name,
+									id : nick,
+									content : $("#m").val(),
+									isPrivate : isPrivate
+								})// type:fileSelected.type
+								$('#d').html("");
+								fileSelected = undefined;
+							}
+							$("#m").val("");
+							$('#smilies').hide();
+							return false;
+						});
+
+						// ///////////////////////
+
+						$('#bFileUpload').on("click", function() {
+							$('#f').click();
+							return false;
+						});
+
+						$('#bSmilies').on("click", function() {
+							console.log("smilies");
+							$('#smilies').toggle();
+							return false;
+						});
+
+						$('.smilies').on("click", function() {
+							var smilie = $(this).attr('id');
+							var chatmessage = $('#m').val();
+							$('#m').val(chatmessage + smilie);
+						})
+
+						$('#onlineUserWidget')
+								.on(
+										"click",
+										".onlineUser",
+										function() {
+											var user = $(this).attr('id');
+											var chatmessage = $('#m').val();
+											$('#m').val(
+													"\\pm " + user + " "
+															+ chatmessage);
+										})
+					});
+
+	socket.on('nickTaken', function(data) {
+		alert("Username already taken!");
+	});
+
+	socket.on('invalidNick', function(data) {
+		alert("Enter a valid username\nOnly letters and numbers allowed");
+	});
+
+	// onclick function for the file upload
+	function FileChosen(event) {
+		fileSelected = event.target.files[0]
+		$('#d').html("File Selected: " + fileSelected.name);
+	}
+
+	socket.on('OnlineUserWidget', function(msg) {
+		var OUWUsers = "Online Users:<br>";
+		for (var i = 0; i < msg.content.length; i++) {
+			OUWUsers += '<span class=onlineUser id=' + msg.content[i] + '>'
+					+ msg.content[i] + '</span><br>';
+		}
+
+		console.log("ouw");
+		console.log(msg.content);
+		console.log(OUWUsers);
+		$('#onlineUserWidget').empty();
+		$('#onlineUserWidget').append(OUWUsers);
+	})
+
+});
