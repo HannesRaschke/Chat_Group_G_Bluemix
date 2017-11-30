7a014be99e053f5cc1cf6cfd9949c9ba1ea91bc8
HannesRaschke <hannes.raschke@student.reutlingen-university.de>
2017-11-30 17:53:47 +0100

    cors support

--- index.js
+++ index.js
@@ -15,23 +15,6 @@ var request = require('request');
 var Cloudant = require('cloudant');
 var VisualRecognitionV3 = require('watson-developer-cloud/visual-recognition/v3');
 
-//cors
-app.use(function(req, res, next) {
-	  res.header("Access-Control-Allow-Origin", "*");
-	  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-	  next();
-	});
-
-	app.get('/', function(req, res, next) {
-	  // Handle the get for this route
-	});
-
-	app.post('/', function(req, res, next) {
-	 // Handle the post for this route
-	});
-
-//
-
 
 ////////////////////
 //cloudant
@@ -87,6 +70,23 @@ app.get('/', function(req, res) {
 });
 
 app.use(express.static(path.join(__dirname, 'public')));
+
+//cors
+app.use(function(req, res, next) {
+	  res.header("Access-Control-Allow-Origin", "*");
+	  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+	  next();
+	});
+
+	app.get('/', function(req, res, next) {
+	  // Handle the get for this route
+	});
+
+	app.post('/', function(req, res, next) {
+	 // Handle the post for this route
+	});
+
+//
 
 
 // //////////////////////////////////////////////////////////////
